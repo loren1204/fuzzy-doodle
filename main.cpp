cf9c182565b0220ce0c312fb4374c02f6585e8e1
loren1204 <166956027+loren1204@users.noreply.github.com>
2024-11-07 23:07:49 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,16 +1,91 @@
-
 #include <iostream>
-#include "LoanData.h"
-#include <iomanip>
 #include <cmath>
+#include <iomanip>
 
-int main()
+class LoanData
+ {
+private:
+    double Bal;
+    double n;
+    double i;
+    double A;
+
+public:
+    LoanData(double p, double n, double i)
+     {
+        i = i / 100.0;      
+        i = i / 12.0;       
+        n = n * 12;         
+        A = p * (i * pow(1 + i, n)) / (pow(1 + i, n) - 1);  
+        Bal = p;            
+    }
+
+    void MakePayment(double pay) 
+    {
+        Bal = (i + 1) * Bal - pay;  
+    }
+
+    void printAmortizationSchedule()
+     {
+        double beginningBalance = Bal;
+        double payment;
+        double interestPaid;
+        double principalPaid;
+        double endingBalance;
+        int month = 1;
+
+        std::cout << std::left
+                  << std::setw(10) << "Month"
+                  << std::setw(20) << "Beginning Balance"
+                  << std::setw(10) << "Payment"
+                  << std::setw(15) << "Interest Paid"
+                  << std::setw(15) << "Principal Paid"
+                  << std::setw(20) << "Ending Balance" << "\n";
+
+        while (beginningBalance > 0)
+         {
+            interestPaid = i * beginningBalance;
+            if ((i + 1) * beginningBalance > A)
+             {
+                payment = A;
+            } else
+             {
+                payment = (i + 1) * beginningBalance;
+            }
+            principalPaid = payment - interestPaid;
+            endingBalance = (i + 1) * beginningBalance - payment;
+            beginningBalance = endingBalance;
+
+            std::cout << std::fixed << std::setprecision(2)
+                      << std::setw(10) << month++
+                      << std::setw(20) << beginningBalance
+                      << std::setw(10) << payment
+                      << std::setw(15) << interestPaid
+                      << std::setw(15) << principalPaid
+                      << std::setw(20) << endingBalance << "\n";
+        }
+    }
+
+    void printPayOffTimeReport(double p, double i) 
+    {
+        double A;
+        for (int n = 6; n < 360; n++) 
+        {
+            A = p * (i * pow(1 + i, n)) / (pow(1 + i, n) - 1);
+            std::cout << "Months: " << n << " - Monthly Payment: " << A << "\n";
+        }
+    }
+};
+
+int main() 
 {
-    std::cout << "Please enter loan amount" << std::endl;
-    std::cout << "Please enter years paying" << std::endl;
-    std::cout << "Please enter loan rate" << std::endl;
+    double p = 10000;   // Loan amount
+    double n = 30;      //years
+    double i = 6;       //rate
 
-    LoanData L1(100000, 30, 6);
-    L1.PrintAmortizationSchedule();
-    L1.PrintPayoffTimeReport(100000, 6);
+    LoanData loan(p, n, i);
+    loan.printAmortizationSchedule();  // Print amortization schedule
+    loan.printPayOffTimeReport(p, i); // Print payoff time report
+
+    return 0;
 }
